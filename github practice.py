b62b186aad7aabd20b9271f92fcf9e5169d4e079
choms_sub218 <127467830+choms218Sub@users.noreply.github.com>
2023-03-15 16:04:40 +0900

    Update github practice.py

--- github practice.py
+++ github practice.py
@@ -1,1 +1,19 @@
-print("name : 조미서, 학번 : 202203298, 학과: 컴퓨터공학")
+// 202203298 컴퓨터공학부 조미서
+
+#include <stdio.h>
+
+int main(){
+  
+printf("이름 : 조미서, 학번 : 202203298, 학과: 컴퓨터공학Boo");
+  
+  return 0;
+}
+
+# #include <iostream>      
+# using namespace std;     
+ 
+# int main(void)          
+# {
+#     cout << "이름 : 조미서, 학번 : 202203298, 학과: 컴퓨터공학Boo" << endl; 
+  
+#     return 0; 
